a9d4571682eaf71e864a4d8a91ec5000bfc83fd3
cmdnodens <rgolobaj80@gmail.com>
2023-11-05 20:28:44 +0200

    Add project files.

---
+++ DCTTask/CoinCapParse.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DCTTask
+{
+    public class CoinCapResponse
+    {
+        public CoinCapData data { get; set; }
+        public double timestamp { get; set; }
+    }
+    public class CoinCapData
+    {
+        public string id { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string priceUsd { get; set; }
+        public string volumeUsd24Hr { get; set; }
+        public string changePercent24Hr { get; set; }
+    }
+
+
+
+    sealed class CoinNameAssets
+    {
+        public List<CoinNameData> data { get; set; }
+    }
+    sealed class CoinNameData
+    {
+        public string id { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+    }
+   
+
+    public class CoinCapParse
+    {
+        private static List<string> coinNames { get; set; }
+        private static async Task<List<string>> GetCoinNames()
+        {
+            //Sending http request and getting response from api
+            var apiUrl = "https://api.coincap.io/v2/assets";
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(apiUrl);
+
+            //Converting response into the json and returning list of coins
+            var assets = JsonSerializer.Deserialize<CoinNameAssets>(response);
+
+            return assets.data.Select(x => x.id).ToList();
+        }
+
+        public static async Task<List<CoinCapData>> GetCoinData(int currentPage, int amount)
+        {
+            var result = new List<CoinCapData>();
+            if (coinNames == null)
+                coinNames = await GetCoinNames();
+            HttpClient httpClient = new HttpClient();
+
+            // Calculate the start and end indices for the current page
+            int startIndex = (currentPage - 1) * amount;
+            int endIndex = startIndex + amount;
+
+            // Ensure endIndex does not exceed the total number of coinNames
+            endIndex = Math.Min(endIndex, coinNames.Count);
+
+            // Fetch data for the coins in the specified range
+            var coinsToFetch = coinNames.GetRange(startIndex, endIndex - startIndex);
+
+            foreach (var coin in coinsToFetch)
+            {
+                var apiUrl = $"https://api.coincap.io/v2/assets/{coin}";
+                var response = await httpClient.GetStringAsync(apiUrl);
+
+                CoinCapData coinData = JsonSerializer.Deserialize<CoinCapResponse>(response).data;
+
+                if (coinData != null)
+                    result.Add(coinData);
+            }
+            return result;
+        }
+
+    }
+
+
+}
---
+++ DCTTask/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace DCTTask
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer dataUpdateTimer;
+        private List<CoinCapData> cryptoData;
+        private int currentPage = 1;
+        private int itemsPerPage = 10; // Change this value as needed
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+
+            // Initialize and start the data update timer
+            dataUpdateTimer = new DispatcherTimer();
+            dataUpdateTimer.Interval = TimeSpan.FromSeconds(5); // Update every 5 seconds
+            dataUpdateTimer.Tick += DataUpdateTimer_Tick;
+            dataUpdateTimer.Start();
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Initial data load
+            await LoadDataAndDisplay();
+        }
+
+        private async void DataUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            // Periodically update data
+            await LoadDataAndDisplay();
+        }
+
+        private async Task LoadDataAndDisplay()
+        {
+            cryptoData = await CoinCapParse.GetCoinData(currentPage, itemsPerPage);
+            UpdateDataGrid();
+        }
+
+        private async void UpdateDataGrid()
+        {
+            cryptoDataGrid.ItemsSource = await CoinCapParse.GetCoinData(currentPage, itemsPerPage);
+        }
+
+
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                UpdateDataGrid();
+            }
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+           // if (currentPage < Math.Ceiling((double)cryptoData.Count / itemsPerPage))
+            
+                currentPage++;
+                UpdateDataGrid();
+            
+        }
+
+
+        private void cryptoDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Handle selection changes
+        }
+    }
+}
